87d74d8ee413a0d666108644a45a2005e5e688f9
clem <clemens.cords@aol.com>
2022-02-07 17:11:54 +0100

    added module proxy

---
+++ .src/module.cpp
@@ -0,0 +1,89 @@
+// 
+// Copyright 2022 Clemens Cords
+// Created on 07.02.22 by clem (mail@clemens-cords.com)
+//
+
+#include <include/module.hpp>
+
+namespace jluna
+{
+    Module::Module(jl_module_t* value)
+        : Proxy((jl_value_t*) value, value->name)
+    {}
+
+    Module::Module(jl_module_t* value, std::shared_ptr<ProxyValue>& owner)
+        : Proxy((jl_value_t*) value, owner, value->name)
+    {}
+
+    jl_module_t * Module::get() const
+    {
+        return (jl_module_t*) Proxy::operator const jl_value_t*();
+    }
+
+    Module::operator jl_module_t*()
+    {
+        return get();
+    }
+
+    jl_sym_t* Module::name() const
+    {
+        get()->name;
+    }
+
+    Module Module::parent() const
+    {
+        return Module(get()->parent);
+    }
+
+    size_t Module::build_id() const
+    {
+        return get()->build_id;
+    }
+
+    std::pair<size_t, size_t> Module::get_uuid() const
+    {
+        return std::pair<size_t, size_t>(get()->uuid.hi, get()->uuid.lo);
+    }
+
+    bool Module::is_top_module() const
+    {
+        return get()->istopmod;
+    }
+
+    int8_t Module::optimization_level() const
+    {
+        return get()->optlevel;
+    }
+
+    int8_t Module::compile_status() const
+    {
+        return get()->compile;
+    }
+
+    std::vector<Symbol> Module::bindings() const
+    {
+        std::vector<Symbol> out;
+
+        auto htable = get()->bindings;
+        for (size_t i = 0; i < htable.size / 2; ++i)
+        {
+            auto key = htable.table[2*i];
+
+            if ((size_t) key != 0x1)
+                out.push_back(Symbol((jl_sym_t*) key));
+        }
+
+        return out;
+    }
+
+    std::vector<Module> Module::usings() const
+    {
+        std::vector<Module> out;
+
+        auto array = jl_main_module->usings;
+        for (size_t i = 0; i < array.len; ++i)
+            out.push_back(Module((jl_module_t*) array.items[i]));
+
+        return out;
+    }
+}
---
+++ include/module.hpp
@@ -0,0 +1,78 @@
+// 
+// Copyright 2022 Clemens Cords
+// Created on 07.02.22 by clem (mail@clemens-cords.com)
+//
+
+#pragma once
+
+#include <julia.h>
+#include <include/proxy.hpp>
+#include <include/symbol.hpp>
+
+namespace jluna
+{
+    // wraps jl_module_t*
+    class Module : public Proxy
+    {
+        public:
+            /// @brief ctor
+            /// @param value
+            /// @param name: symbol
+            Module(jl_module_t*);
+
+            /// @brief ctor as owned
+            /// @param value
+            /// @param owner: internal proxy value owner
+            /// @param name: symbol
+            Module(jl_module_t* value, std::shared_ptr<ProxyValue>& owner);
+
+            /// @brief decay to C-type
+            operator jl_module_t*();
+
+            /// @brief wrap c-property name
+            /// @returns name as symbol
+            jl_sym_t* name() const;
+
+            /// @brief wrap c-property parent
+            /// @returns parent as module, nullptr if topmod
+            Module parent() const;
+
+            /// @brief wrap hidden c-property build_id
+            /// @returns size_t
+            size_t build_id() const;
+
+            /// @brief wrap hidden c-property uuid
+            /// @returns uuid where .first = hi; .second = lo;
+            std::pair<size_t, size_t> get_uuid() const;
+
+            /// @brief wrap hidden c-property istopmod
+            /// @returns bool
+            bool is_top_module() const;
+
+            /// @brief wrap hidden c-property optlevel
+            /// @returns level as int
+            int8_t optimization_level() const;
+
+            /// @brief wrap hidden c-property compile
+            /// @returns status : -1, 0, 1
+            int8_t compile_status() const;
+
+            /// @brief wrap hidden c-property bindings
+            /// @returns hashtable where keys are symbols and values are the memory symbols are bound to
+            std::vector<Symbol> bindings() const;
+
+            /// @brief wrap hidden c-property usings
+            /// @returns list of modules declared
+            std::vector<Module> usings() const;
+
+        private:
+            jl_module_t* get() const;
+
+
+
+
+
+        private:
+            jl_module_t* any;
+    };
+}
