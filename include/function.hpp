2b14d445e724e6ae87aa1c376755f049d79025cc
clem <clemens.cords@aol.com>
2022-02-09 02:26:14 +0100

    added function header

---
+++ include/function.hpp
@@ -0,0 +1,20 @@
+// 
+// Copyright 2022 Clemens Cords
+// Created on 09.02.22 by clem (mail@clemens-cords.com)
+//
+
+#pragma once
+
+#include <include/proxy.hpp>
+
+namespace jluna
+{
+    class Function : public Proxy
+    {
+        public:
+            // invokable with
+            // invoke result
+            // methods
+
+    };
+}
