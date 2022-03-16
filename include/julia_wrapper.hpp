aee8cc281390dbf96ec0f6b83111dd060e28b225
friendlyanon <friendlyanon@users.noreply.github.com>
2022-03-16 20:50:05 +0100

    Fix the comment style in `julia_wrapper.hpp`

--- include/julia_wrapper.hpp
+++ include/julia_wrapper.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
 #ifdef _WIN32
-/* julia.h includes Windows headers that are not self contained, so this
- * wrapper header includes Windows.h before includeing julia.h, so everything
- * is setup for the other headers to actually work */
+// julia.h includes Windows headers that are not self contained. This wrapper
+// header includes Windows.h before including julia.h, so everything is setup
+// for the other headers to actually work.
 # define NOMINMAX
 # define WIN32_LEAN_AND_MEAN
 # include <Windows.h>
